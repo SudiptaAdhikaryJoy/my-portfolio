c74b630321c12a91d1aff68d02573d079e0b2c04
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-06-16 01:04:02 +0600

    add typically npm package

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import React from 'react'
 import { AiOutlineMail } from 'react-icons/ai';
 import {FaGithub, FaInstagram, FaLinkedinIn} from 'react-icons/fa';
+import Typical from 'react-typical';
 
 export const Main = () => {
   return (
@@ -12,7 +13,19 @@ export const Main = () => {
                 <h1 className='py-4 text-gray-700'>
                     Hello, I'm <span className="text-[#5651e5]">Sudipta Adhikary</span>
                 </h1>
-                <h1 className='py-2 text-gray-700'>A Full Stack Developer</h1>
+                <h1 className='py-2 text-gray-700'>
+                    <Typical
+                loop={ Infinity}
+                steps={[
+                    "Full Stack Developer",
+                    500, 
+                    "MERN Stack Developer",
+                    500, 
+                    "Backend Stack Developer",
+                    400, 
+                ]}
+                 />
+                    </h1>
                 <p className='py-4 text-gray-600 mx-w-[70%] m-auto'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ducimus officiis illo ipsam obcaecati voluptatibus eaque eveniet assumenda, impedit iusto!</p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
