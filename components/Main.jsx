82dc180bd9dd8a637db88a2fedc0e4056db9d19f
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-06-15 13:10:07 +0600

    skill section added

---
+++ components/Main.jsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import React from 'react'
+import { AiOutlineMail } from 'react-icons/ai';
+import {FaGithub, FaInstagram, FaLinkedinIn} from 'react-icons/fa';
+
+export const Main = () => {
+  return (
+    <div className='w-full h-screen text-center'>
+        <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+            <div>
+                <p className='uppercase text-sm tracking-widest text-gray-600'>Lets Build Something Together</p>
+                <h1 className='py-4 text-gray-700'>
+                    Hello, I'm <span className="text-[#5651e5]">Sudipta Adhikary</span>
+                </h1>
+                <h1 className='py-2 text-gray-700'>A Full Stack Developer</h1>
+                <p className='py-4 text-gray-600 mx-w-[70%] m-auto'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ducimus officiis illo ipsam obcaecati voluptatibus eaque eveniet assumenda, impedit iusto!</p>
+                <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <FaLinkedinIn />
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <FaGithub />
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineMail />
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <FaInstagram />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
