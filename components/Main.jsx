5a0b43aa0c82feaba7bd055c6ef884ed9f0b40d6
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-06-16 18:21:32 +0600

    typical implement

--- components/Main.jsx
+++ components/Main.jsx
@@ -13,19 +13,19 @@ export const Main = () => {
                 <h1 className='py-4 text-gray-700'>
                     Hello, I'm <span className="text-[#5651e5]">Sudipta Adhikary</span>
                 </h1>
-                <h1 className='py-2 text-gray-700'>
+                <h2 className='py-2 text-gray-800'>
                     <Typical
                 loop={ Infinity}
                 steps={[
-                    "Full Stack Developer",
+                    "Full Stack Engineer",
                     500, 
                     "MERN Stack Developer",
                     500, 
-                    "Backend Stack Developer",
+                    "Backend Developer",
                     400, 
                 ]}
                  />
-                    </h1>
+                    </h2>
                 <p className='py-4 text-gray-600 mx-w-[70%] m-auto'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ducimus officiis illo ipsam obcaecati voluptatibus eaque eveniet assumenda, impedit iusto!</p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
