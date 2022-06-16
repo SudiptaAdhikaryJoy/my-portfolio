d6b6465f68f0446596604c24250a92f8dd0297d8
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-06-16 18:40:11 +0600

    button added

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -50,7 +50,28 @@ const Contact = () => {
                                         </label>
                                             <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" />
                                     </div>
+                                    <div className='flex flex-col'>
+                                        <label className='uppercase text-sm py-2'>Number
+                                        </label>
+                                            <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" />
+                                    </div>
                                 </div>
+                                <div className='flex flex-col py-2'>
+                                    <label className='uppercase text-sm py-2'>Email
+                                        </label>
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="email" />
+                                </div>
+                                <div className='flex flex-col py-2'>
+                                    <label className='uppercase text-sm py-2'>Subject
+                                        </label>
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" />
+                                </div>
+                                <div className='flex flex-col py-2'>
+                                    <label className='uppercase text-sm py-2'>Message
+                                        </label>
+                                        <textarea className='border-2 rounded-lg p-3 border-gray-300' rows="10" />
+                                </div>
+                                <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
                             </form>
                         </div>
                     </div>
