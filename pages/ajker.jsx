796bceae6819a276c2071abf11ee8bd9c0a9a778
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-06-29 23:49:47 +0600

    add

--- pages/ajker.jsx
+++ pages/ajker.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import propertyOne from '../public/assets/property/propertyOne.png'
+import {RiRadioButtonFill} from 'react-icons/ri'
 
 const ajker = () => {
   return (
@@ -10,7 +11,7 @@ const ajker = () => {
         <Image className='absolute z-1' layout='fill' objectFit='cover' src={propertyOne} alt='' />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-y-[-50%] translate-x-[-50%] z-10 py-6 px-6 text-white'>
           <h2 className='py-2'>Ajker Barta</h2>
-          <h3>An Online Base News Portal</h3>
+          <h3>An Online Base News Portal!</h3>
         </div>
       </div>
 
@@ -18,13 +19,22 @@ const ajker = () => {
         <div className='col-span-4'>
           <h4>Project</h4>
           <h2>Overview</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum magnam tenetur, totam laboriosam ipsum atque iure animi aspernatur. Molestiae, repudiandae?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum magnam tenetur, totam laboriosam ipsum atque iure animi aspernatur. Molestiae, repudiandae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum necessitatibus eos dicta, doloremque esse consequuntur magnam pariatur quidem. Aliquid consequuntur dolores quasi odio ratione suscipit excepturi quisquam eligendi vel aut, quas ea dolorum exercitationem rem cupiditate quia eius temporibus eveniet voluptas, quaerat repudiandae deleniti, reprehenderit earum! Quis voluptates accusantium quibusdam!</p>
           <button className='px-8 py-2 mt-4 mr-9'>Demo</button>
           <button className='px-8 py-2 mt-4'>Code</button>
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl rounded-xl shadow-gray-400 p-4'>
           <div className='p-2'>
             <p className='text-center font-bold pb-2'>Technologies</p>
+            <div>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> React</p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> NextJs</p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Tailwind CSS</p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> MongoDB</p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> NodeJs</p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Firebase</p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> NPM Packages</p>
+            </div>
           </div>
           <div>
 
